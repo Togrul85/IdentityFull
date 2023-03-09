bcf7618dd4ee3feb28739c1219569116c00c8d07
Togrul85 <toghrulkhs@code.edu.az>
2023-03-09 18:06:39 +0400

    ..

---
+++ FrontToBack2/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using FrontToBack2.Models;
+using FrontToBack2.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontToBack2.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Register(RegisterVM register)
+        {
+            if (!ModelState.IsValid) return View();
+
+            AppUser user = new();
+            user.Email = register.Email;
+            user.Fullname= register.Fullname;
+            user.UserName = register.Fullname;
+
+
+
+            return View();
+            
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
